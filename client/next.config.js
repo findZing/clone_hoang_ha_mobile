efa484ee772aa933d5dbb3d7ef7779fcdbb85fe2
findZing <manhnguyenhuu1001@gmail.com>
2023-01-11 14:41:49 +0700

    third

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    serverUrl: 'http://localhost:5000/'
-  },
+  // env: {
+  //   // serverUrl: 'http://localhost:5000/'
+  // },
   images: {
     domains: ['cdn.hoanghamobile.com','hoanghamobile.com'],
   },
