71e7605892161e94a2b1f96be878bd5f4df90ee2
findZing <manhnguyenhuu1001@gmail.com>
2023-01-11 22:26:29 +0700

    Add router

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -4,6 +4,7 @@ import authController from '../controllers/authController/auth'
 
 const router = express.Router()
 
+router.post('/requestrefreshtoken', authController.requestRefreshToken)
 router.post('/register', authController.registerUser)
 router.post('/login', authController.loginUser)
 router.post('/logout', authController.logOutUser)
